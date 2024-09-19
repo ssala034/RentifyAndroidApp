f1f842320387f861ff4420f6b86b571a114837bb
emilyjette <emily.m.jette@gmail.com>
2024-09-19 10:41:55 -0400

    Made LoginActivity functuional activity with xml file

--- Rentify/app/src/main/java/com/group20/rentify/activity/LoginActivity.java
+++ Rentify/app/src/main/java/com/group20/rentify/activity/LoginActivity.java
@@ -1,4 +1,15 @@
 package com.group20.rentify.activity;
 
-public class LoginActivity  {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.group20.rentify.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+    }
 }
--- Rentify/app/src/main/java/com/group20/rentify/activity/MainActivity.java
+++ Rentify/app/src/main/java/com/group20/rentify/activity/MainActivity.java
@@ -1,19 +1,7 @@
 package com.group20.rentify.activity;
 
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.group20.rentify.databinding.ActivityMainBinding;
 
 import com.group20.rentify.R;
 
