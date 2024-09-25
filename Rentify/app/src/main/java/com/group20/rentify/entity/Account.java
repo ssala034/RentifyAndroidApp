dc234e9e3af28ab01ee5deb222345d60bd234342
Xuan Pham <hpham024@uottawa.ca>
2024-09-25 07:17:05 -0400

    created constructors for account classes

--- Rentify/app/src/main/java/com/group20/rentify/entity/Account.java
+++ Rentify/app/src/main/java/com/group20/rentify/entity/Account.java
@@ -1,5 +1,7 @@
 package com.group20.rentify.entity;
 
+import androidx.annotation.NonNull;
+
 import java.util.ArrayList;
 
 public class Account implements Entity {
@@ -15,4 +17,39 @@ public class Account implements Entity {
     private String email;
     private String role;  // this may be changed to a different type later
 
+    // constructors
+
+    /**
+     * Minimum argument constructor.
+     * @param username  The unique identifier for the user
+     * @param email     The primary email of the user
+     * @param role      One of {admin, renter, lessor}
+     */
+    public Account(String username, String email, String role) {
+        this(username, email, role, "Anonymous");
+    }
+
+    /**
+     * Full argument constructor.
+     * @param username  The unique identifier for the user
+     * @param email     The primary email of the user
+     * @param role      One of {admin, renter, lessor}
+     * @param name      The name or nickname of the user
+     */
+    public Account(String username, String email, String role, String name) {
+        this.username = username;
+        this.role = role;
+        this.name = name;
+        this.email = email;
+    }
+
+    /**
+     * Copy constructor
+     */
+    public Account(@NonNull Account a) {
+        this.username = a.username;
+        this.role = a.role;
+        this.name = a.name;
+        this.email = a.email;
+    }
 }
--- Rentify/app/src/main/java/com/group20/rentify/entity/AdminAccount.java
+++ Rentify/app/src/main/java/com/group20/rentify/entity/AdminAccount.java
@@ -1,8 +1,25 @@
 package com.group20.rentify.entity;
 
+import androidx.annotation.NonNull;
+
 import java.util.ArrayList;
 
 public class AdminAccount extends Account {
+
+    public AdminAccount(String username, String email) {
+        super(username, email, "admin");
+    }
+
+    public AdminAccount(String username, String email, String name) {
+        super(username, email, "admin", name);
+    }
+
+    /**
+     * Create an admin version of account.
+     */
+    public AdminAccount(@NonNull Account a) {
+        super(a.getUsername(), a.getEmail(), "admin", a.getName());
+    }
 
     /**Return the account readable attributes.
      * Instead of keeping all accounts as a master list in memory,
