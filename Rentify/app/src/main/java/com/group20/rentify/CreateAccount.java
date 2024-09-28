e4688fdb91b49274f21f0f79a02975952bb26ce5
emilyjette <emily.m.jette@gmail.com>
2024-09-27 21:28:44 -0400

    added button function and empty activities

--- Rentify/app/src/main/java/com/group20/rentify/CreateAccount.java
+++ Rentify/app/src/main/java/com/group20/rentify/CreateAccount.java
@@ -1,41 +1,24 @@
 package com.group20.rentify;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class CreateAccount extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_create_account);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-    public void signInClick(@NonNull View view){
-        // validate email and password
-        Intent intent = new Intent(this, WelcomeActivity.class);
-        startActivity(intent);
-    }
-
-    public void createAccountClick(@NonNull View view){
-        // go to create account activity
-        Intent intent = new Intent(this, CreateAccount.class);
-        startActivity(intent);
-    }
-    
 }
