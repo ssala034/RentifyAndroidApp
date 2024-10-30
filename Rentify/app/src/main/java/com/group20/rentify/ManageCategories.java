7f7e47c486b782cbec8b3c7cee98c38b66921b76
emilyjette <emily.m.jette@gmail.com>
2024-10-30 14:37:53 -0400

    properly implemented styles for manage category

--- Rentify/app/src/main/java/com/group20/rentify/ManageCategories.java
+++ Rentify/app/src/main/java/com/group20/rentify/ManageCategories.java
@@ -1,13 +1,22 @@
 package com.group20.rentify;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.LayoutInflaterCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+
+import com.group20.rentify.databinding.ActivityManageCategoriesBinding;
 
 public class ManageCategories extends AppCompatActivity {
 
@@ -15,23 +24,23 @@ public class ManageCategories extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        TestBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_manage_categories);
-        binding.textCategoryTitle.setText("hello");
-
+        setContentView(R.layout.activity_manage_categories);
+        LinearLayout parentLayout = findViewById(R.id.categoriesListView);
+        addCategoryView(parentLayout,"Test1","hello i am test 1");
+        addCategoryView(parentLayout,"test2", "hello i am test 2!!");
 
     }
 
-    private void addText(String text){
-        binding.textCategoryTitle.setText("hello world");
+    private void addCategoryView(LinearLayout parentLayout, String title, String description){
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View categoryView = inflater.inflate(R.layout.manage_category,parentLayout,false);
+
+        TextView categoryTitle = categoryView.findViewById(R.id.textCategoryTitle);
+        TextView categoryDescription = categoryView.findViewById(R.id.textCategoryDescription);
+
+        categoryTitle.setText(title);
+        categoryDescription.setText(description);
+
+        parentLayout.addView(categoryView);
     }
-
-    /*
-     * TestBinding binding = DataBindingUtil.setContentView(this, activity_manage_categories);
-     * binding.textCategoryTitle.setText("hello world");
-     *
-     * */
-
-    //HelloWorldBinding binding =
-    //    DataBindingUtil.setContentView(this, R.layout.hello_world);
-    //binding.hello.setText("Hello World");
 }
