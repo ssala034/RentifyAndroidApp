3e5a09aa8aed6ad3616490defe3fcd2f13811261
emilyjette <emily.m.jette@gmail.com>
2024-10-30 15:10:12 -0400

    finished methods that need to be adjusted to work better with the database

--- Rentify/app/src/main/java/com/group20/rentify/ManageCategories.java
+++ Rentify/app/src/main/java/com/group20/rentify/ManageCategories.java
@@ -25,22 +25,26 @@ public class ManageCategories extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_manage_categories);
-        LinearLayout parentLayout = findViewById(R.id.categoriesListView);
-        addCategoryView(parentLayout,"Test1","hello i am test 1");
-        addCategoryView(parentLayout,"test2", "hello i am test 2!!");
+        //test of addCategoryView, replace with proper
+        LinearLayout parent = findViewById(R.id.categoriesListView);
+        addCategoryView(parent,"Test1","hello i am test 1");
+        addCategoryView(parent,"test2", "hello i am test 2!!");
+        addCategoryView(parent,"test3", "hello i am test 3!!!");
 
     }
 
     private void addCategoryView(LinearLayout parentLayout, String title, String description){
+        //Use this method when creating new categories to get correct layout
         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
         View categoryView = inflater.inflate(R.layout.manage_category,parentLayout,false);
 
+        //individual text, title and description
         TextView categoryTitle = categoryView.findViewById(R.id.textCategoryTitle);
         TextView categoryDescription = categoryView.findViewById(R.id.textCategoryDescription);
 
         categoryTitle.setText(title);
         categoryDescription.setText(description);
-
+        //add to layout
         parentLayout.addView(categoryView);
     }
 }
--- Rentify/app/src/main/java/com/group20/rentify/ViewAllCategories.java
+++ Rentify/app/src/main/java/com/group20/rentify/ViewAllCategories.java
@@ -1,6 +1,10 @@
 package com.group20.rentify;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,29 @@ public class ViewAllCategories extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_view_all_categories);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        LinearLayout parent = findViewById(R.id.categoriesListView);
+        addCategoryView(parent,"Test1","i am test 1");
+        addCategoryView(parent,"Test","i am a test");
+        addCategoryView(parent,"Test","i am a test");
+
+
+
+    }
+
+
+    private void addCategoryView(LinearLayout parentLayout, String title, String description){
+        //Use this method when creating new categories to get correct layout
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View categoryView = inflater.inflate(R.layout.view_category,parentLayout,false);
+
+        //individual text, title and description
+        TextView categoryTitle = categoryView.findViewById(R.id.categoryTitle);
+        TextView categoryDescription = categoryView.findViewById(R.id.categoryDetails);
+
+        categoryTitle.setText(title);
+        categoryDescription.setText(description);
+        //add to layout
+        parentLayout.addView(categoryView);
     }
 }
