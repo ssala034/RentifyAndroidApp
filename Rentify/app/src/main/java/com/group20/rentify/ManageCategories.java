2711f24c44815abb5c82ecae9f4c4b264d7f1368
emilyjette <emily.m.jette@gmail.com>
2024-10-31 14:37:09 -0400

    finished delete implementation

--- Rentify/app/src/main/java/com/group20/rentify/ManageCategories.java
+++ Rentify/app/src/main/java/com/group20/rentify/ManageCategories.java
@@ -3,6 +3,7 @@ package com.group20.rentify;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -20,51 +21,62 @@ import com.group20.rentify.entity.Category;
 public class ManageCategories extends AppCompatActivity {
 
     private boolean isManaging = false;
-    private List<Category> categories;
+    private final List<Category> categories = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_manage_categories);
-        test();
-        //test of addCategoryView, replace with proper
-        //addCategory("Test1","hello i am test 1","01");
+        //parent cannot be a local variable, has to be in on create
         LinearLayout parent = findViewById(R.id.categoriesListView);
-        for (Category category : categories) {
-            addCategoryView(parent, category.getName(), category.getDescription());
+
+        for(int i = 0; i < 20; i++){
+            addCategory();
         }
 
         Button manage = findViewById(R.id.buttonManageCategories);
         manage.setOnClickListener(this::onManageCategoriesButtonClicked);
 
+        ImageView delete = parent.findViewById(R.id.buttonDeleteCategory);
+        delete.setOnClickListener(this:: onDeleteButtonClicked);
+
+
     }
+
+
+
     private void onManageCategoriesButtonClicked(View view) {
         Button button = (Button) view;
         //does not have functionality rn so commented as to not break code
         //List<Category> categories = controller.getCategories();
         if(isManaging){
-            button.setText("Manage Categories");
+            button.setText(R.string.text_manage_categories);
             ImageView edit = findViewById(R.id.buttonEditCategory);
             edit.setVisibility(ImageView.GONE);
             isManaging = false;
         } else{
-            button.setText("Done");
+            button.setText(R.string.text_done);
             ImageView edit = findViewById(R.id.buttonEditCategory);
             edit.setVisibility(ImageView.VISIBLE);
+
 
             isManaging = true;
         }
 
     }
-    protected void addCategoryView(LinearLayout parentLayout, String title, String description){
+    private void addCategoryView(LinearLayout parentLayout,String title, String description){
+        Category currCategory = categories.get(categories.size() - 1);
+
+
         //Use this method when creating new categories to get correct layout
         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
         View categoryView = inflater.inflate(R.layout.view_screen,parentLayout,false);
-        //setting up delete button tag
+
         ImageView delete = categoryView.findViewById(R.id.buttonDeleteCategory);
-        Category currCategory = categories.get(categories.size() - 1);
         delete.setTag(currCategory);
+        delete.setOnClickListener(this:: onDeleteButtonClicked);
+
 
         //individual text, title and description
         TextView categoryTitle = categoryView.findViewById(R.id.textName);
@@ -73,32 +85,30 @@ public class ManageCategories extends AppCompatActivity {
         categoryTitle.setText(title);
         categoryDescription.setText(description);
 
-        delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                deleteCategoryView(v, parentLayout);
-            }
-        });
+
         //add to layout
         parentLayout.addView(categoryView);
     }
 
-    private void deleteCategoryView(View view, LinearLayout parent){
-        Category categoryToDelete = (Category) view.getTag();
+    private void onDeleteButtonClicked(View view) {
+        int id = view.getId();
+        Category tag = (Category) view.getTag();
+        View category = (View) view.getParent().getParent();
+        ViewGroup parentLayout = (ViewGroup) category.getParent();
+        parentLayout.removeView(category);
+        categories.remove(tag);
 
-        View categoryView = (View) view.getParent();
-        parent.removeView(categoryView);
-
-        categories.remove(categoryToDelete);
 
     }
 
-    private void test(){
-       categories = new ArrayList<>();
-       Category category;
-       for(int i = 0; i < 5; i++){
-           category = new Category("Test " +i, "hello", "0"+i);
-           categories.add(category);
-       }
+    private void addCategory(){
+        int i = categories.size();
+        Category newOne = new Category("Test" +i, "hello", "0"+i);
+        categories.add(newOne);
+        String title = newOne.getName();
+        String description = newOne.getDescription();
+        LinearLayout parent = findViewById(R.id.categoriesListView);
+        addCategoryView(parent,title,description);
+
     }
 }
