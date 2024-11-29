8c33dbd57ec91263ad87d8fa32a8558f7f429df8
Lining Ao <Nicolea25418@gmail.com>
2024-11-28 23:05:30 -0500

    overall UI change across the app. Implemented dark mode settings but will need to create a dark mode button to toggle setting. Basically the text and background settings allow for darkmode integration but the features is not completed.

--- Rentify/app/src/main/java/com/group20/rentify/AdminDashboard.java
+++ Rentify/app/src/main/java/com/group20/rentify/AdminDashboard.java
@@ -17,6 +17,7 @@ public class AdminDashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_dashboard);
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
